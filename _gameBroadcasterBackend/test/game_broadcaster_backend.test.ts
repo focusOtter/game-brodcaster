b6978764b6ae12452c6e7caf827fef89414c7d93
focusotter <mtliendo@focusotter.com>
2024-01-29 21:26:57 -0600

    init project

---
+++ _gameBroadcasterBackend/test/game_broadcaster_backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GameBroadcasterBackend from '../lib/game_broadcaster_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/game_broadcaster_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GameBroadcasterBackend.GameBroadcasterBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
